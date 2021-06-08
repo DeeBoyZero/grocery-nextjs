c8f8c7d75cba19bb6893c470b2ad3c3314d81acf
Mathieu Desilets <mathieu.desilets@gmail.com>
2021-06-08 15:18:07 -0400

    Force Deploy

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import MainNavigation from "../components/MainNavigation";
 import SimpleList from "../components/SimpleList";
